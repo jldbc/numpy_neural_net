d860fa43e1287e047c1be4250118c9b5f46b2c15
James LeDoux <ledouxja@bc.edu>
2016-12-19 22:28:00 -0800

    simplify network and implement backprop

--- feed_forward_network.py
+++ feed_forward_network.py
@@ -1,63 +1,80 @@
-import numpy as np
+import numpy as np 
 import math
+from sklearn import datasets
 
-def activation_function(x):
-	return np.tanh(x)
+def calculate_loss(model,X,y,reg_lambda):
+    num_examples = X.shape[0]
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation to calculate our predictions
+    z1, a1, z2, out = feed_forward(model, X)
+    probs = out / np.sum(out, axis=1, keepdims=True)
+    # Calculating the loss
+    corect_logprobs = -np.log(probs[range(num_examples), y])
+    loss = np.sum(corect_logprobs)
+    # Add regulatization term to loss (optional)
+    loss += reg_lambda/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    return 1./num_examples * loss
 
-def softmax(x):
-	pass
+# Helper function to predict an output (0 or 1)
+def feed_forward(model, x):
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    out = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return z1, a1, z2, out
+    #return np.argmax(out, axis=1)
 
-def sigmoid(x):
-	return 1 / (1 + np.exp(-x))
+def build_model(X,hidden_nodes,output_dim=2):
+    model = {}
+    input_dim = X.shape[1]
+    model['W1'] = np.random.randn(input_dim, hidden_nodes) / np.sqrt(input_dim)
+    model['b1'] = np.zeros((1, hidden_nodes))
+    model['W2'] = np.random.randn(hidden_nodes, output_dim) / np.sqrt(hidden_nodes)
+    model['b2'] = np.zeros((1, output_dim))
+    return model
 
-def loss(yhat, y, measure):
-	if measure == "mse":
-		mse = np.square(y-yhat).mean()
-		return mse
-	if measure == "cross_entropy":
-		print "oops, haven't implemented this yet"
-
-
-def feed_forward(X, y, nodes=[16,8], n_iter=1000, model_type="regression"):
-	nodes.append(1) #single node for final output
- 	n_obs = X.shape[0]
-	depth = len(nodes)
-	#w needs to know dim of previous layer for initialization
-	prev_dim = [X.shape[1]]
-	for i in range(depth-1):
-		prev_dim.append(nodes[i])
-	#weights: list of weight matrices. (n_nodes for prev. layer) x (n_nodes for next layer).
-	weights = [np.random.rand(1,nodes[i]) for i in range(depth)]
-	weights = [np.repeat(weights[i],prev_dim[i],axis=0) for i in range(depth)]
-	#bias: n_layers x 1 array. A scalar bias value for each layer.
-	bias = np.asarray([np.random.rand(1) for i in range(depth)])
-	#train the network
-	for i in range(n_iter):
-		h = X #initial nodes are the column values for each observation
-		for k in range(depth):
-			if k != depth:
-				a = h.dot(weights[k]) + bias[k] #(layer input * weights) + bias node = activation input
-				h = activation_function(a) #activation(Wx + b) = next layer's input
-			elif k == depth: #different activation for the output layer
-				a = h.dot(weights[k]) + bias[k] #no additional function if regression. softmax if classification.
-				if model_type == "classification":
-					h = softmax(a)
-				elif model_type == "regression":
-					h = a
-	iter_loss = loss(h, y, measure="mse")
-	print iter_loss
-	return h
-
-def backprop():
-	pass
+def train(model, X, y, num_passes=10000, reg_lambda = .1, learning_rate=0.1, print_loss=False):
+    # Batch gradient descent
+    for i in xrange(0, num_passes):
+        z1,a1,z2,output = feed_forward(model, X)
+        # Backpropagation
+        delta3 = output
+        delta3[range(X.shape[0]), y] -= 1  #yhat - y
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(model['W2'].T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+        # Add regularization terms
+        dW2 += reg_lambda * model['W2']
+        dW1 += reg_lambda * model['W1']
+        # Gradient descent parameter update
+        model['W1'] -= learning_rate * dW1
+        model['b1'] -= learning_rate * db1
+        model['W2'] -= learning_rate * dW2
+        model['b2'] -= learning_rate * db2
+        if print_loss and i % 1000 == 0:
+          print "Loss after iteration %i: %f" %(i, calculate_loss(model, X, y, reg_lambda))
+    return model
 
 
 def main():
-	X = np.linspace(0,10,100)
-	y = [np.sin(i) for i in X]
-	X = np.asmatrix(X).T
-	y = np.asmatrix(y).T
-	feed_forward(X,y,nodes=[128,64, 32, 16])
+	#toy dataset
+	X, y = datasets.make_moons(20, noise=0.10)
+	num_examples = len(X) # training set size
+	nn_input_dim = 2 # input layer dimensionality
+	nn_output_dim = 2 # output layer dimensionality 
+	learning_rate = 0.01 # learning rate for gradient descent
+	reg_lambda = 0.01 # regularization strength
+	model = build_model(X,8,2)
+	model = train(model,X, y, reg_lambda=reg_lambda, learning_rate=learning_rate,print_loss=True)
+	output = feed_forward(model, X)
+	preds = np.argmax(output[3], axis=1)
+
+main()
 
 
 
